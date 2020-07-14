e5b76bec098f070bdcb5bbf727a82ed8df5f42d8
mvieitesdiaz <mvieites@cern.ch>
2020-07-14 13:18:58 +0200

    testwebhook

--- solution.py
+++ solution.py
@@ -5,7 +5,7 @@ import sys
 import pandas as pd
 
 def predict(data):
-    """ get two colums from `df` and returns the predicted ouput as a column (pd.Series)"""
+    """ get two colums from `df` and returns the predicted ouput as a column (pd.Series) """
     result = data['col1'] + data['col2']
     return result
 
